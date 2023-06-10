fd706d1cd513f5b986c6b4cda12f0cc93dda3a03
Joao Lino <linusmannuel@gmail.com>
2023-06-10 23:00:23 +0100

    fetch de livros no console

---
+++ app/main.js
@@ -0,0 +1,13 @@
+const endpointDaAPI =
+	'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+let livros = [];
+
+async function getBuscarLivos() {
+	const res = await fetch(endpointDaAPI);
+	livros = await res.json();
+	console.table(livros);
+	return livros;
+}
+
+getBuscarLivos();
